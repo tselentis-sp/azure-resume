2c765b8534ac0b66e8455dd40b292e65fcc22e61
tselentis-sp <tselentis.sp@gmail.com>
2024-04-13 22:44:14 +0300

    Created azure function api

---
+++ backend/api/Counter.cs
@@ -0,0 +1,17 @@
+//using Microsoft.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace Company.Function
+{
+
+    public class Counter
+    {
+        [JsonProperty(PropertyName ="id")]
+
+        public string Id {get; set;}
+
+        [JsonProperty(PropertyName = "count")]
+
+        public int Count {get; set;}
+    }
+}
--- frontend/main.js
+++ frontend/main.js
@@ -1,19 +1,22 @@
+
 window.addEventListener('DOMContentLoaded', (event)=>{
     getVisitCount();
 })
 
-const functionApi = '';
+const functionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
-        return response.json{}
-    }).then(response =>{
-        console.log("Website called function API.");
-        count = response.count;
-        document.getElementById("counter").innerText = count;
-    }).catch(function(error){
-        console.log(error);
-    })
-    return count;
+    fetch(functionApi)
+        .then(response => response.json())
+        .then(data =>{
+            console.log("Website called function API.");
+            console.log(data);
+            count = data.count;
+            document.getElementById("counter").innerText = count;
+            console.log(data.json);
+        }).catch(function(error){
+            console.log(error);
+        })
+        return count;
 }    
