5dc8581982edd4483df9ec52b4c3d28e2917846d
tselentis-sp <tselentis.sp@gmail.com>
2024-06-10 22:08:56 +0300

    Updates on the backend workflow

---
+++ backend/tests/TestCounter.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Data.Common;
+using System.Net.Http;
+using System.Text;
+
+
+// namespace tests
+
+public class TestCounter
+{   
+    // private readonly ILogger logger = TestFactory.CreateLogger();
+
+    [Fact]
+    public async void Http_trigger_should_known_string()
+    {
+        var counter = new Company.Function.Counter();
+        counter.Id = "1";
+        counter.Count = 2;
+        var request = TestFactory.CreateHttpRequest();
+        var response = (HttpResponseMessage) Company.Function.GetResumeCounter.Run(request, counter, out counter, logger);
+        Asser.Equal(3, counter.Count);   
+    }
+}
